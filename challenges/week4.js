5a992122425dec0f0a2896fb487a898f42f6cd5b
Saba Nouman <sabanuk03@gmail.com>
2019-11-30 14:33:48 +0000

    js test week 4 completed

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,43 +1,93 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  const smallNumbers = nums.filter(number => {
+    return number < 1;
+  });
+  return smallNumbers;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   // Your code here
+  const findNames = names.filter(function (name) {
+    if (name[0] === char) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return findNames
+
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
+  const verbsFound = words.filter(function (word) {
+    const findTo = (word.slice(0, 3));
+    if (findTo === "to ") {
+      return true
+    } else {
+      return false
+    }
+  })
+  return verbsFound;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  let areIntegers = [];
+  nums.forEach(function (eachNums) {
+    if (Number.isInteger(eachNums)) {
+      areIntegers.push(eachNums)
+    }
+  })
+  return areIntegers
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
+  const userCities = users.map(function (user) {
+    const userData = user.data.city.displayName
+    return userData
+  })
+  return userCities
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  const squareRoots = nums.map(n => {
+    return Math.round(Math.sqrt(n) * 100) / 100
+  });
+  return squareRoots
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   // Your code here
+  let resultSentences = [];
+  sentences.forEach(function (sentence) {
+    if (sentence.toLowerCase().includes(str)) {
+      resultSentences.push(sentence)
+    }
+  })
+  return resultSentences
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   // Your code here
+  let longestSides = [];
+  triangles.forEach(function (triangle) {
+    longestSides.push(Math.max(...triangle));
+  })
+  return longestSides
 }
 
 module.exports = {
